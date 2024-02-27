3b0ffa054dda3695f48624143e197a5f22bbf396
parkerrappleye <parker.rappleye1@gmail.com>
2024-02-26 20:36:21 -0700

    bug fixes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function generateSvg(answers) {
     }
 
     // Calculate the font size to fit within the shape
-    const fontSize = Math.min(250, 200 / answers.text.length);
+    const fontSize = Math.min(300, 250 / answers.text.length);
 
     // Create the SVG content
     const svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n${shapeElement}\n<text x="50%" y="50%"  font-size="${fontSize}" dominant-baseline="middle" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text>\n</svg>`;
